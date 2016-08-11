12de053f5fc46b9bb749497d6936d7159c2392af
jlong299 <jlong299@foxmail.com>
2016-08-11 16:33:47 +0800

    Selection of 1 trb or 16 trbs. Not compiled

---
+++ turbo_rtl/trb_out_mux_num1.v
@@ -0,0 +1,347 @@
+//-----------------------------------------------------------------
+// Module Name:        	trb_out_mux.v
+// Project:             NLB AFU turbo decoder
+// Description:         Mux arbiter consist of  NUM_TURBO FIFOs
+// Author:				Long Jiang
+//
+//---------------------------------------------------------------------------------------
+//  st in ---> FIFO ------> Arbiter/Mux --> st out
+//                     |
+//  st in ---> FIFO -->
+//                 ...
+//                     |
+//  st in ---> FIFO -->
+//
+//----------------------------------------------------------------------------------------
+// !!!
+// Only work when fix turbo length = 1024
+
+module trb_out_mux_num1 #(parameter
+	NUM_TURBO = 2
+	)
+(
+	input 			rst_n, 	 // sync to clk !		
+	input 			clk,	
+
+	input [7:0]						st_data_in [NUM_TURBO-1 :0] ,
+	input [NUM_TURBO-1 :0]			st_valid_in,
+	input [NUM_TURBO-1 :0]			st_sop_in,
+	input [NUM_TURBO-1 :0]			st_eop_in,
+	output reg	[NUM_TURBO-1 :0]	st_ready_out,
+
+	input 				st_ready_in,
+	output reg	[7:0]	st_data_out,
+	output reg			st_valid_out,
+	output reg			st_sop_out,
+	output reg			st_eop_out,
+
+	output reg	[NUM_TURBO-1 : 0] 	err_fifo_state,  //if (fifo empty) && (rden == 1) 
+	output reg	[NUM_TURBO-1 : 0] 	err_fifo_full   
+	);
+
+
+localparam ST_LEN = 128; // turbo_length/8
+reg 		read_one_frame_done;
+reg [5 : 0]  		num_frame_in_fifo [NUM_TURBO-1 : 0] ;
+reg [10 : 0] 		cnt_rden [NUM_TURBO-1 : 0] ;
+reg [NUM_TURBO-1 : 0]				rden;
+
+reg	[7:0]					st_data_q [NUM_TURBO-1 : 0] ;
+reg	[NUM_TURBO-1 : 0] 		st_valid_q;
+reg	[NUM_TURBO-1 : 0] 		st_sop_q;
+reg	[NUM_TURBO-1 : 0] 		st_eop_q;
+
+reg	[NUM_TURBO-1 : 0] 		empty;
+
+reg 	[3:0] 	st_out_fsm;
+
+reg	[1:0]					cnt_rden_wait [NUM_TURBO-1 : 0] ;
+
+
+genvar i;
+generate 
+for (i=0; i<NUM_TURBO; i=i+1)
+begin: gen_test
+
+	// FIFOs inst
+	fifo_st_data fifo_st_data_inst (
+	  .data  (st_data_in[i]), 
+	  .wrreq (st_valid_in[i]),
+	  .rdreq (rden[i]),
+	  .clock (clk),
+	  .sclr  (!rst_n), 
+	  .q     (st_data_q[i]),    
+	  .usedw (),
+	  .full  (err_fifo_full[i]), 
+	  .empty (empty[i]) 
+	);
+
+	
+	always@(posedge clk)
+	begin
+		if (!rst_n)
+		begin
+			num_frame_in_fifo[i] <= 0;
+			cnt_rden[i] <= 0;
+			rden[i] <= 0;
+			cnt_rden_wait[i] <= 0;
+		end
+		else
+		begin
+			// If number of frames in fifo less then 4, st_ready_out=1'b1
+			if (num_frame_in_fifo[i] <= 6'd4)
+				st_ready_out[i] <= 1'b1;
+			else 
+				st_ready_out[i] <= 1'b0;
+
+			// Number of turbo frames in fifo. Every clk +1, -1, or maintain
+			if ( cnt_rden[i] == ST_LEN )
+				if (st_eop_in[i])
+					num_frame_in_fifo[i] <= num_frame_in_fifo[i];
+				else
+					num_frame_in_fifo[i] <= num_frame_in_fifo[i] - 6'd1;
+			else if (st_eop_in[i])
+				num_frame_in_fifo[i] <= num_frame_in_fifo[i] + 6'd1;
+			else
+				num_frame_in_fifo[i] <= num_frame_in_fifo[i];
+
+			// Set rden length = ST_lEN
+			// cnt_rden_wait[i] :  disable rden for several clks after st_out_fsm = i, in order to kill a bug
+			if (cnt_rden_wait[i] != 2'd3)
+			begin
+				cnt_rden[i] <= 0;
+				rden[i] <= 0;
+			end
+			else
+			begin
+				if ( (st_out_fsm == i) && (num_frame_in_fifo[i] != 0) && st_ready_in )
+				begin
+					cnt_rden[i] <= ( cnt_rden[i] == ST_LEN ) ? 11'd0 : cnt_rden[i] + 11'd1;
+					rden[i] <= ( cnt_rden[i] != 11'd0 );
+				end
+				else
+				begin
+					cnt_rden[i] <= 0;
+					rden[i] <= 0;
+				end
+			end
+
+			if (st_out_fsm == i)
+			begin
+				if ( cnt_rden_wait[i] != 2'd3)
+					cnt_rden_wait[i] <= cnt_rden_wait[i] + 2'd1;
+				else
+					cnt_rden_wait[i] <= 2'd3;
+			end
+			else
+				cnt_rden_wait[i] <= 2'd0;
+
+		end
+	end
+
+	// Generate sop,eop,valid  according to rden
+	always@(posedge clk)
+	begin
+		if (!rst_n)
+		begin
+			st_valid_q[i] <= 0;
+			st_sop_q[i] <= 0;
+			st_eop_q[i] <= 0;
+			err_fifo_state[i] <= 0;
+		end
+		else
+		begin
+			st_valid_q[i] <= rden[i];
+			st_sop_q[i] <= rden[i] & (!st_valid_q[i]) ;
+			st_eop_q[i] <= (cnt_rden[i] == 11'd0) && (rden[i] == 1'b1) ;
+			err_fifo_state[i] <= empty[i] & rden[i];
+		end
+	end
+
+end
+endgenerate
+
+
+//-------- FSM : represent one mux branch ----------
+always@(posedge clk)
+begin
+	if (!rst_n)
+	begin
+		st_out_fsm <= 0;
+	end
+	else
+	begin
+		if ( read_one_frame_done )
+			st_out_fsm <= (st_out_fsm == NUM_TURBO-1) ? 4'd0 : st_out_fsm + 4'd1;
+		else
+			st_out_fsm <= st_out_fsm;
+	end
+end
+
+
+//-----------------------------------------------
+//start----- Rewrite if NUM_TURBO change --------
+//-----------------------------------------------
+always@(posedge clk)
+begin
+	if (!rst_n)
+	begin
+		read_one_frame_done <= 0;
+	end
+	else
+	begin
+		read_one_frame_done <= 	( cnt_rden[0] == ST_LEN ); //|
+								// ( cnt_rden[1] == ST_LEN ) |
+								// ( cnt_rden[2] == ST_LEN ) |
+								// ( cnt_rden[3] == ST_LEN ) |
+								// ( cnt_rden[4] == ST_LEN ) |
+								// ( cnt_rden[5] == ST_LEN ) |
+								// ( cnt_rden[6] == ST_LEN ) |
+								// ( cnt_rden[7] == ST_LEN ) |
+								// ( cnt_rden[8] == ST_LEN ) |
+								// ( cnt_rden[9] == ST_LEN ) |
+								// ( cnt_rden[10] == ST_LEN ) |
+								// ( cnt_rden[11] == ST_LEN ) |
+								// ( cnt_rden[12] == ST_LEN ) |
+								// ( cnt_rden[13] == ST_LEN ) |
+								// ( cnt_rden[14] == ST_LEN ) |
+								// ( cnt_rden[15] == ST_LEN ) 
+								//;
+	end
+end
+
+reg [3:0] st_out_fsm_q;
+always@(posedge clk)		
+begin
+	st_out_fsm_q <= st_out_fsm;
+end
+
+always@(*)
+begin
+	case (st_out_fsm_q)
+	4'd0:
+	begin
+		st_data_out <= st_data_q[0];
+		st_valid_out <= st_valid_q[0];
+		st_sop_out <= st_sop_q[0];
+		st_eop_out <= st_eop_q[0];
+	end
+	// 4'd1:
+	// begin
+	// 	st_data_out <= st_data_q[1];
+	// 	st_valid_out <= st_valid_q[1];
+	// 	st_sop_out <= st_sop_q[1];
+	// 	st_eop_out <= st_eop_q[1];
+	// end
+	// 4'd2:
+	// begin
+	// 	st_data_out <= st_data_q[2];
+	// 	st_valid_out <= st_valid_q[2];
+	// 	st_sop_out <= st_sop_q[2];
+	// 	st_eop_out <= st_eop_q[2];
+	// end
+	// 4'd3:
+	// begin
+	// 	st_data_out <= st_data_q[3];
+	// 	st_valid_out <= st_valid_q[3];
+	// 	st_sop_out <= st_sop_q[3];
+	// 	st_eop_out <= st_eop_q[3];
+	// end
+	// 4'd4:
+	// begin
+	// 	st_data_out <= st_data_q[4];
+	// 	st_valid_out <= st_valid_q[4];
+	// 	st_sop_out <= st_sop_q[4];
+	// 	st_eop_out <= st_eop_q[4];
+	// end
+	// 4'd5:
+	// begin
+	// 	st_data_out <= st_data_q[5];
+	// 	st_valid_out <= st_valid_q[5];
+	// 	st_sop_out <= st_sop_q[5];
+	// 	st_eop_out <= st_eop_q[5];
+	// end
+	// 4'd6:
+	// begin
+	// 	st_data_out <= st_data_q[6];
+	// 	st_valid_out <= st_valid_q[6];
+	// 	st_sop_out <= st_sop_q[6];
+	// 	st_eop_out <= st_eop_q[6];
+	// end
+	// 4'd7:
+	// begin
+	// 	st_data_out <= st_data_q[7];
+	// 	st_valid_out <= st_valid_q[7];
+	// 	st_sop_out <= st_sop_q[7];
+	// 	st_eop_out <= st_eop_q[7];
+	// end
+	// 4'd8:
+	// begin
+	// 	st_data_out <= st_data_q[8];
+	// 	st_valid_out <= st_valid_q[8];
+	// 	st_sop_out <= st_sop_q[8];
+	// 	st_eop_out <= st_eop_q[8];
+	// end
+	// 4'd9:
+	// begin
+	// 	st_data_out <= st_data_q[9];
+	// 	st_valid_out <= st_valid_q[9];
+	// 	st_sop_out <= st_sop_q[9];
+	// 	st_eop_out <= st_eop_q[9];
+	// end
+	// 4'd10:
+	// begin
+	// 	st_data_out <= st_data_q[10];
+	// 	st_valid_out <= st_valid_q[10];
+	// 	st_sop_out <= st_sop_q[10];
+	// 	st_eop_out <= st_eop_q[10];
+	// end
+	// 4'd11:
+	// begin
+	// 	st_data_out <= st_data_q[11];
+	// 	st_valid_out <= st_valid_q[11];
+	// 	st_sop_out <= st_sop_q[11];
+	// 	st_eop_out <= st_eop_q[11];
+	// end
+	// 4'd12:
+	// begin
+	// 	st_data_out <= st_data_q[12];
+	// 	st_valid_out <= st_valid_q[12];
+	// 	st_sop_out <= st_sop_q[12];
+	// 	st_eop_out <= st_eop_q[12];
+	// end
+	// 4'd13:
+	// begin
+	// 	st_data_out <= st_data_q[13];
+	// 	st_valid_out <= st_valid_q[13];
+	// 	st_sop_out <= st_sop_q[13];
+	// 	st_eop_out <= st_eop_q[13];
+	// end
+	// 4'd14:
+	// begin
+	// 	st_data_out <= st_data_q[14];
+	// 	st_valid_out <= st_valid_q[14];
+	// 	st_sop_out <= st_sop_q[14];
+	// 	st_eop_out <= st_eop_q[14];
+	// end
+	// 4'd15:
+	// begin
+	// 	st_data_out <= st_data_q[15];
+	// 	st_valid_out <= st_valid_q[15];
+	// 	st_sop_out <= st_sop_q[15];
+	// 	st_eop_out <= st_eop_q[15];
+	// end
+	default:
+	begin
+		st_data_out <= 0;
+		st_valid_out <= 0;
+		st_sop_out <= 0;
+		st_eop_out <= 0;
+	end
+	endcase
+end
+//---------------------------------------------
+//end----- Rewrite if NUM_TURBO change --------
+//---------------------------------------------
+
+endmodule
---
+++ turbo_rtl/turbo_d_all_num1.v
@@ -0,0 +1,264 @@
+//-----------------------------------------------------------------
+// Module Name:        	turbo_d_all.v
+// Project:             NLB AFU turbo decoder
+// Description:         Packed NUM_TURBO turbo decoders into one module
+// Author:				Long Jiang
+//
+//---------------------------------------------------------------------------------------
+//  bus in --> arbiter demux ----> bus2st --> turbo ----> trb_out_mux --> st out
+//                            |                        |
+//                            ---> bus2st --> turbo -->
+//                                    ...
+//                            |                        |
+//                            ---> bus2st --> turbo -->
+//
+//----------------------------------------------------------------------------------------
+// !!!
+// Only work when fix turbo length = 1024
+
+module turbo_d_all_num1 #(parameter 
+	BUS=534,
+	ST=8 
+	)
+(
+	input 					rst_n,  // clk_bus Asynchronous reset active low
+	
+	input 					clk_bus,    // Clock 400MHz
+	input [BUS-1:0]			bus_data,
+	input					bus_en,
+	output	reg				bus_ready,
+
+	input 					clk_st,
+	input			 		st_ready,
+	output [ST-1:0] 		st_data,
+	output					st_valid,
+	output					st_sop,
+	output					st_eop
+	//output					st_error, 
+
+);
+
+localparam NUM_TURBO = 1;
+localparam NUM_BUS_PER_TURBO_PKT=25;
+
+reg [3:0] 		bus2st_rdy_fsm;
+reg [NUM_TURBO-1 : 0] 	bus_ready_r;			
+reg [8:0]  		cnt_bus_en;
+
+reg [BUS-1:0] 	bus_data_r [0:0] /* synthesis keep */;
+
+//reg [NUM_TURBO-1 : 0] 	bus_en_r;
+
+//start-------- only for NUM_TURBO = 1 -----------
+reg [0 : 0] 	bus_en_r;			
+reg [0 : 0] 	bus_en_rr;	
+//end  -------- only for NUM_TURBO = 1 -----------		
+reg [3:0] 		bus2st_rdy_fsm_r;
+ 
+//---------------------------------------
+//start--------- Arbiter ----------------
+//-- Description: mux/demux of "ready" and "en" signal
+//--              one root  <---> NUM_TURBO branches     
+//----------------------------------------
+always@(posedge clk_bus)
+begin
+	if (!rst_n)
+	begin
+		bus2st_rdy_fsm <= 0;
+		bus2st_rdy_fsm_r <= 0;
+		bus_en_r <= 0;
+		bus_en_rr <= 0;
+		cnt_bus_en <= 0;
+		bus_ready <= 0;
+	end
+	else
+	begin
+		//-----------------------------------------------
+		//start----- Rewrite if NUM_TURBO change --------
+		//-----------------------------------------------
+		case (bus2st_rdy_fsm)
+		4'd0:
+			bus_ready <= bus_ready_r[0];
+
+		default:
+			bus_ready <= 0;
+		endcase
+		 
+		// bus_en_r[0] <= (bus2st_rdy_fsm == 4'd0) ? bus_en : 1'b0;
+		// bus_en_r[1] <= (bus2st_rdy_fsm == 4'd1) ? bus_en : 1'b0;
+		// bus_en_r[2] <= (bus2st_rdy_fsm == 4'd2) ? bus_en : 1'b0;
+		// bus_en_r[3] <= (bus2st_rdy_fsm == 4'd3) ? bus_en : 1'b0;
+		// bus_en_r[4] <= (bus2st_rdy_fsm == 4'd4) ? bus_en : 1'b0;
+		// bus_en_r[5] <= (bus2st_rdy_fsm == 4'd5) ? bus_en : 1'b0;
+		// bus_en_r[6] <= (bus2st_rdy_fsm == 4'd6) ? bus_en : 1'b0;
+		// bus_en_r[7] <= (bus2st_rdy_fsm == 4'd7) ? bus_en : 1'b0;
+		// bus_en_r[8] <= (bus2st_rdy_fsm == 4'd8) ? bus_en : 1'b0;
+		// bus_en_r[9] <= (bus2st_rdy_fsm == 4'd9) ? bus_en : 1'b0;
+		// bus_en_r[10] <= (bus2st_rdy_fsm == 4'd10) ? bus_en : 1'b0;
+		// bus_en_r[11] <= (bus2st_rdy_fsm == 4'd11) ? bus_en : 1'b0;
+		// bus_en_r[12] <= (bus2st_rdy_fsm == 4'd12) ? bus_en : 1'b0;
+		// bus_en_r[13] <= (bus2st_rdy_fsm == 4'd13) ? bus_en : 1'b0;
+		// bus_en_r[14] <= (bus2st_rdy_fsm == 4'd14) ? bus_en : 1'b0;
+		// bus_en_r[15] <= (bus2st_rdy_fsm == 4'd15) ? bus_en : 1'b0;
+
+		//start-------- only for NUM_TURBO = 16 -----------
+		bus_en_r[0] <= (bus2st_rdy_fsm[3:2] == 2'd0) ? bus_en : 1'b0;
+
+		bus_en_rr[0] <= (bus2st_rdy_fsm_r[1:0] == 2'd0) ? bus_en_r[0] : 1'b0;
+
+		//end  -------- only for NUM_TURBO = 16 -----------
+
+		//---------------------------------------------
+		//end----- Rewrite if NUM_TURBO change --------
+		//---------------------------------------------
+
+		if (  cnt_bus_en == NUM_BUS_PER_TURBO_PKT-1  && bus_en == 1'b1 )
+			bus2st_rdy_fsm <= ( bus2st_rdy_fsm == NUM_TURBO-1) ? 4'd0 : bus2st_rdy_fsm + 4'd1;
+		else
+			bus2st_rdy_fsm <= bus2st_rdy_fsm;
+
+		if (bus_en)
+			cnt_bus_en <= (cnt_bus_en == NUM_BUS_PER_TURBO_PKT-1) ? 9'd0 : cnt_bus_en + 9'd1;
+		else
+			cnt_bus_en <= cnt_bus_en;
+
+		bus2st_rdy_fsm_r <= bus2st_rdy_fsm;
+	end
+end
+//end--------- Arbiter ---------------------
+//------------------------------------------
+
+
+//start-------- bus2st & turbo ----------------
+reg [NUM_TURBO-1 : 0]		trb_source_valid;
+reg [NUM_TURBO-1 : 0]		trb_source_ready;
+reg [NUM_TURBO-1 : 0]		trb_source_sop;
+reg [NUM_TURBO-1 : 0]		trb_source_eop;
+reg [7:0] 		trb_source_data_s [NUM_TURBO-1 :0] ;
+reg  		rst_n_clk_st;
+
+
+reg  rst_n_q, rst_n_qq;
+always@(posedge clk_st)
+begin
+	rst_n_clk_st <= rst_n_qq;
+	rst_n_qq <= rst_n_q;
+	rst_n_q <= rst_n;
+end
+
+// genvar i;
+// generate 
+// for (i=0; i<NUM_TURBO; i=i+1)
+// begin: test
+
+// 	bus2st_turbo  #(
+//     .BUS (534),
+//     .ST_PER_BUS (512),
+//     .NUM_ST_PER_BUS (42), //  (ST_PER_BUS / ST)
+//     .ST_PER_TURBO_PKT (1028),   // 1024+4
+//     .NUM_BUS_PER_TURBO_PKT (25),
+//     .ST (12)
+//   	)
+// 	bus2st_turbo_inst(
+// 	  .rst_n            (rst_n),             
+// 	  .clk_bus         	(clk_bus),
+// 	  .bus_data			(bus_data),
+// 	  .bus_en 			(bus_en_r[i]),
+// 	  .bus_ready 		(bus_ready_r[i]),
+
+// 	  //.rst_n_out 		(rst_n_clk_st), // output , rst of clk_st domain
+
+// 	  .clk_st 			(clk_st),
+	  
+// 	  .source_valid    (trb_source_valid[i]   ),   // source.source_valid
+// 	  .source_ready    (trb_source_ready[i]   ),   //       .source_ready
+// 	  //.source_error    (   ),   //       .source_error
+// 	  .source_sop      (trb_source_sop[i]     ),   //       .source_sop
+// 	  .source_eop      (trb_source_eop[i]     ),   //       .source_eop
+// 	  //.crc_pass        (    ),   //       .crc_pass
+// 	  //.crc_type        (    ),   //       .crc_type
+// 	  //.source_iter     (    ),   //       .source_iter
+// 	  //.source_blk_size (	),   //       .source_blk_size
+// 	  .source_data_s   (trb_source_data_s[i]  )    //       .source_data_s
+// 	);
+// end
+// endgenerate
+
+
+//start--------- only for NUM_TURBO = 16 ---------------
+always@(posedge clk_bus)
+begin
+	bus_data_r[0] <= bus_data;
+	bus_data_r[1] <= bus_data;
+	bus_data_r[2] <= bus_data;
+	bus_data_r[3] <= bus_data;
+end
+
+
+genvar i;
+generate 
+for (i=0; i<1; i=i+1)
+begin: test0
+
+	bus2st_turbo  #(
+    .BUS (534),
+    .ST_PER_BUS (512),
+    .NUM_ST_PER_BUS (42), //  (ST_PER_BUS / ST)
+    .ST_PER_TURBO_PKT (1028),   // 1024+4
+    .NUM_BUS_PER_TURBO_PKT (25),
+    .ST (12)
+  	)
+	bus2st_turbo_inst(
+	  .rst_n            (rst_n),             
+	  .clk_bus         	(clk_bus),
+	  .bus_data			(bus_data_r[0]),
+	  .bus_en 			(bus_en_rr[i]),
+	  .bus_ready 		(bus_ready_r[i]),
+
+	  //.rst_n_out 		(rst_n_clk_st), // output , rst of clk_st domain
+
+	  .clk_st 			(clk_st),
+	  
+	  .source_valid    (trb_source_valid[i]   ),   // source.source_valid
+	  .source_ready    (trb_source_ready[i]   ),   //       .source_ready
+	  //.source_error    (   ),   //       .source_error
+	  .source_sop      (trb_source_sop[i]     ),   //       .source_sop
+	  .source_eop      (trb_source_eop[i]     ),   //       .source_eop
+	  //.crc_pass        (    ),   //       .crc_pass
+	  //.crc_type        (    ),   //       .crc_type
+	  //.source_iter     (    ),   //       .source_iter
+	  //.source_blk_size (	),   //       .source_blk_size
+	  .source_data_s   (trb_source_data_s[i]  )    //       .source_data_s
+	);
+end
+endgenerate
+
+
+//end  --------- only for NUM_TURBO = 16 ---------------
+
+//end-------- bus2st & turbo ----------------
+
+
+//start--------- trb_out_mux ----------------
+trb_out_mux #( .NUM_TURBO (NUM_TURBO) )
+trb_out_mux_inst
+(
+	.rst_n 			(rst_n_clk_st), //!!! to be connected
+	.clk 			(clk_st),
+
+	.st_data_in 	(trb_source_data_s),
+	.st_valid_in 	(trb_source_valid),
+	.st_sop_in 		(trb_source_sop),
+	.st_eop_in 		(trb_source_eop),
+	.st_ready_out 	(trb_source_ready),
+
+	.st_ready_in 	(st_ready),
+	.st_data_out 	(st_data),
+	.st_valid_out 	(st_valid),
+	.st_sop_out 	(st_sop),
+	.st_eop_out 	(st_eop)
+	);
+//end--------- trb_out_mux ----------------
+
+
+endmodule
